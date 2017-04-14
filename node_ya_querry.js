a8f74bbc584cdb436aeb2119f369ae5f3e235331
mixxxxx <spartacusxxxxx@gmail.com>
2017-04-15 00:03:07 +0300

    fffd commit

--- node_ya_querry.js
+++ node_ya_querry.js
@@ -1,61 +1,11 @@
-var Crawler = require("crawler");
-var urllib = require('url');
+//var Crawler = require("crawler");
+//var urllib = require('url');
 var cheerio = require('cheerio');
-var cheerioAdv = require('cheerio-advanced-selectors');
+//var cheerioAdv = require('cheerio-advanced-selectors');
 var request = require("request");
 
  
-var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page 
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
 
-            //var $ = res.$;
-
-
-			//cheerio = cheerioAdv.wrap(cheerio)
-
-			//var html1 = cheerio.load(res);
-			var $ = cheerio.load(res);
-
-
-   			$('span.comhead').each(function(i, element){
-      		var a = $(this).prev();
-      		console.log(a.text());
-    		});
-
-
-            // $ is Cheerio by default 
-            //a lean implementation of core jQuery designed specifically for the server 
-            //console.log($("title").text());
-
-
-            //console.log(html1('div:eq(3)').text());
-
-           //console.log(res);
-
-
-
-        }
-        done();
-    }
-});
- 
-// Queue just one URL, with default callback 
-//c.queue('http://www.amazon.com');
-
-
-// Queue just one URL, with default callback 
-//c.queue('https://www.google.com/search?q=data+mining');
-
-
-
-
-
-//var url = "http://www.wunderground.com/cgi-bin/findweather/getForecast?&query=" + 02888;
 
 var url = "https://www.google.com/search?q=data+mining";
 
@@ -64,11 +14,29 @@ request(url, function (error, response, body) {
     if (!error) {
         var $ = cheerio.load(body),
         links = $(".r a");
-            //bodyhtml = $.html();   //a href="/url?q=
+          
 
-		//var link = $(this.attr('href'));
+        links.each(function (i, link) 
+        {
+			// get the href attribute of each link
+			var url_res = $(link).attr("href");
+		
+			// strip out unnecessary junk
+			url_res = url_res.replace("/url?q=", "").split("&")[0];
+		
+			if (url_res.charAt(0) === "/") {
+				return;
+			}
 
-        console.log(links);
+			console.log(url_res);
+
+		});
+
+
+
+
+
+        //console.log(links);
     } else {
         console.log("Произошла ошибка: " + error);
     }
